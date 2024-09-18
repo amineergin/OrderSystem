23218af74179013df839810dadb65421f461b182
Amine Hatun Ergin <76783053+amineergin@users.noreply.github.com>
2024-09-18 17:03:50 +0300

    Add files via upload

---
+++ CustomerOrders/src/main/java/com/example/qkare/CustomerOrders/Controller/FavoritesController.java
@@ -0,0 +1,31 @@
+package com.example.qkare.CustomerOrders.Controller;
+
+import com.example.qkare.CustomerOrders.Model.Entity.Favorites;
+import com.example.qkare.CustomerOrders.Service.FavoritesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/favorites")
+public class FavoritesController {
+    @Autowired
+    private FavoritesService favoritesService;
+
+    @PostMapping("/createFavList")
+    public ResponseEntity<Favorites> createFavList(@RequestBody Favorites favorites) {
+        Favorites savedFavorites = favoritesService.createFavList(favorites);
+        return ResponseEntity.ok(savedFavorites);
+    }
+
+    @GetMapping("/getAllFavLists")
+    public List<Favorites> getAllFavLists(){
+        return favoritesService.getFavs();
+    }
+}
